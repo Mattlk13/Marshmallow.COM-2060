2862df1d229083c39a98e65d2b68e06c04fc9739
Adam Young <me@adam-young.xyz>
2017-03-07 12:15:35 +0000

    Add Initial Code

---
+++ app/src/main/java/com/teammarshmallow/eventapp/eventplanner/PersonalDetailActivity.java
@@ -0,0 +1,13 @@
+package com.teammarshmallow.eventapp.eventplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+    }
+}
